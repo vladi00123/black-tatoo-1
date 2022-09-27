bd0c351bba984ddf9027c4f524a585355fc9f483
VladimirPinho <55743531+vladi00123@users.noreply.github.com>
2022-09-26 23:12:18 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,3 @@
 <?php
-
-    $dbHost = 'Localhost';
-    $dbUsername = 'root';
-    $dbPassword = 'root';
-    $dbName = 'black-tatoo';
-    
-    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
-
-    // if($conexao->connect_errno)
-    // {
-    //     echo "Erro";
-    // }
-    // else
-    // {
-    //     echo "Conexão efetuada com sucesso";
-    // }
-
+    $mysqli = mysqli_connect("localhost","tatoo","admin","blacktatoo");
 ?>
