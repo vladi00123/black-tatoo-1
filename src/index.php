3bd501b3441a9059e3ecdd868fd282e3d71b81a3
Vicky do nascimento <101911203+Yads689@users.noreply.github.com>
2022-12-20 02:23:57 -0300

    a

--- src/index.php
+++ src/index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>black-tatoo</title>
-    <link rel="stylesheet" href="_css/index.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
 
 </head>
 <body>
