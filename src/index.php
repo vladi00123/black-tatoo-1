2adfdbb07186f68fbd09cee2ad431148f6c2473a
Vicky do nascimento <101911203+Yads689@users.noreply.github.com>
2022-12-20 02:52:56 -0300

    a

--- src/index.php
+++ src/index.php
@@ -5,14 +5,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>black-tatoo</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/index.css">
 
 </head>
 <body>
-    <h1>black tatoo</h1>
-    <div class="box">
-        <a href="login.php">Login</a>
-        <a href="formulario.php">Cadastre-se</a>
-    </div>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light
+    bg-light">
+        <a class="navbar-brand" href="Index.php">Black Tatoo Studio</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data- target="#conteudoNavbarSuportado" aria- controls="conteudoNavbarSuportado" aria- expanded="false" aria-label="Alterna navegação">
+    <span class="navbar-toggler-icon"></span>
+    </button>
+        <div class="collapse navbar-collapse" id="conteudoNavbarSuportado">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tatuagens</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Agendamento</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre nós</a>
+                </li>
+
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <div class=".caixa">
+                    <a href="login.php">Login</a>
+                    <a href="Formulario.php">Cadastre-se</a>
+                </div>
+            </form>
+        </div>
+    </nav>
+</header>
+<div class="container text-center">
+            <img src="_img/logo.jpeg" alt="Logo CP2">
+        </div>
 </body>
 </html>
