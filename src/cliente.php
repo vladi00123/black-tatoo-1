f8663c31851f998c810476e1a087f54f9b81752e
Vicky do nascimento <101911203+Yads689@users.noreply.github.com>
2022-12-21 01:00:21 -0300

    a

--- src/cliente.php
+++ src/cliente.php
@@ -5,9 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
     <title>Document</title>
 </head>
 <body>
+<?php include "header.php" ?>
     <h1>cliente</h1>
 </body>
 </html>
